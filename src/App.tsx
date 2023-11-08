4895e37935e88504f30096e910e92f7557930a49
Diana <sounfair.02@gmail.com>
2023-11-08 12:34:00 +0600

    #6 Implemented layout with page-not-found component, integrated routing in App, added toolbar with logo on the left and navigation menu on the right. Created separate components for authenticated and anonymous users in the navigation menu. Menu updates based on user authentication status. Clicking on the logo navigates to the main page ('/').

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
-function App() {
-	return <></>;
-}
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { CustomLayout } from './components/Layout/Layout';
+import { PageNotFound } from './components/PageNotFound/PageNotFound';
+
+const Home = () => {
+	return <div>Home Page</div>;
+};
+
+const Login = () => {
+	return <div>Login Page</div>;
+};
+
+const App = () => {
+	return (
+		<Router>
+			<CustomLayout>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="*" element={<PageNotFound />} />
+				</Routes>
+			</CustomLayout>
+		</Router>
+	);
+};
 
 export default App;
