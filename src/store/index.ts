62db230b0bbae96983e56e8a22390126a7aeee66
Dmitry <mikotsowl22@gmail.com>
2023-11-13 11:39:32 +0600

    #18 Add redux-persist connection to store
    Added persistConfig settings
    Added persistReducer
    Connected persistReducer to store
    Added persistor export

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,36 @@
 import { rootReducer } from './rootReduser';
 import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import {
+	FLUSH,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+	REHYDRATE,
+	persistReducer,
+	persistStore,
+} from 'redux-persist';
+import userSlice from '../features/user/userSlice';
+
+const persistConfig = {
+	key: 'root',
+	storage,
+	whitelist: [userSlice.name],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-	reducer: rootReducer,
+	reducer: persistedReducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export const persistor = persistStore(store);
