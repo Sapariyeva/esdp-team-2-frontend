692d1fdbbe5d7e017bc30ab9843811bfc9b905ea
Тургумбаев Наиль Рашитович <nailsanti2k19@gmail.com>
2023-12-21 22:52:39 +0600

    #80 "feet(task-80): updated the global layout.

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,26 +1,31 @@
 import { FC } from 'react';
-import { Layout as AntLayout } from 'antd';
-import styles from './Layout.module.scss';
+import { Layout, Layout as AntLayout } from 'antd';
 import { Outlet } from 'react-router-dom';
 import { useAppSelector } from '../../store/hooks';
 import { Toolbar } from '../Toolbar/Toolbar.tsx';
 import { FooterMenu } from '../Footer/Footer.tsx';
 
-const { Header, Content, Footer } = AntLayout;
+const { Content, Footer } = AntLayout;
 
 export const CustomLayout: FC = () => {
 	const logged = useAppSelector((state) => state.users.logged);
 	return (
-		<AntLayout className={styles.layout_container}>
-			<Header className={styles.layout_header}>
-				<Toolbar isAuthenticated={logged} />
-			</Header>
-			<Content className={styles.layout_main_pages}>
-				<Outlet />
+		<Layout>
+			<Toolbar isAuthenticated={logged} />
+			<Content>
+				<Layout
+					style={{
+						padding: '24px 0',
+					}}
+				>
+					<Content style={{ display: 'flex', justifyContent: 'center' }}>
+						<Outlet />
+					</Content>
+				</Layout>
 			</Content>
-			<Footer className={styles.layout_footer}>
+			<Footer style={{ textAlign: 'center' }}>
 				<FooterMenu />
 			</Footer>
-		</AntLayout>
+		</Layout>
 	);
 };
