d231ed39ad224605fb1d0f7fa1c014879ea13ab2
Тургумбаев Наиль Рашитович <nailsanti2k19@gmail.com>
2023-12-22 18:36:38 +0600

    #98 "feet(task-98): fixed the styles, tweaked the layout as well as the content in it

--- src/containers/psychologist/personal_account/PsychologistAccountPage.tsx
+++ src/containers/psychologist/personal_account/PsychologistAccountPage.tsx
@@ -1,36 +1,17 @@
 import { MenuProps, message } from 'antd';
-import { Key, ReactNode, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useAppDispatch } from '../../../store/hooks';
 import styles from '../../patient/personal_account/PatientAccountPage.module.scss';
 import { Outlet, useNavigate } from 'react-router-dom';
 import { logoutUser } from '../../../features/user/userSlice.ts';
-import { CiEdit } from 'react-icons/ci';
-import { GoHistory } from 'react-icons/go';
-import { AiOutlineHeart } from 'react-icons/ai';
-import { IoIosLogOut } from 'react-icons/io';
 import { ActiveTabPatient } from '../../patient/personal_account/PatientAccountPage.tsx';
 import SideBar from '../../../components/SideBar/SideBar.tsx';
-
-type MenuItem = Required<MenuProps>['items'][number];
-
-function getItem(
-	label: ReactNode,
-	key: Key,
-	icon?: ReactNode,
-	children?: MenuItem[],
-	type?: 'group',
-	onClick?: () => void
-): MenuItem {
-	return {
-		key,
-		icon,
-		children,
-		label,
-		type,
-		onClick,
-	};
-}
-
+import getItem from '../../../helpers/getItem.ts';
+import logout from '../../../assets/icon/logout.svg';
+import profile from '../../../assets/icon/profile.svg';
+import calendar from '../../../assets/icon/calendar.svg';
+import record from '../../../assets/icon/record.svg';
+import course from '../../../assets/icon/book-saved.svg';
 const PsychologistAccountPage = () => {
 	const dispatch = useAppDispatch();
 
@@ -47,14 +28,30 @@ const PsychologistAccountPage = () => {
 	};
 
 	const items: MenuProps['items'] = [
-		getItem('Профиль', 'profile', <CiEdit />),
-		getItem('Календарь', 'calendar', <CiEdit />),
-		getItem('Мои клиенты', 'records', <GoHistory />),
-		getItem('Курсы', 'course', <AiOutlineHeart />),
+		getItem(
+			'Профиль',
+			'profile',
+			<img className={styles.img} src={profile} alt="profile" />
+		),
+		getItem(
+			'Календарь',
+			'calendar',
+			<img className={styles.img} src={calendar} alt="calendar" />
+		),
+		getItem(
+			'Мои клиенты',
+			'records',
+			<img className={styles.img} src={record} alt="records" />
+		),
+		getItem(
+			'Курсы',
+			'course',
+			<img className={styles.img} src={course} alt="course" />
+		),
 		getItem(
 			'Выход',
 			'exit',
-			<IoIosLogOut />,
+			<img className={styles.img} src={logout} alt="logout" />,
 			undefined,
 			undefined,
 			handleLogout
