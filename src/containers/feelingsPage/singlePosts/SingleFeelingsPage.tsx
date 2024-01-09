d4922e19aac07650cceaecb9f9d8fe895575aa39
kapusta02 <n.saratovtsev@mail.ru>
2024-01-09 20:50:31 +0600

    #158 loading processing

--- src/containers/feelingsPage/singlePosts/SingleFeelingsPage.tsx
+++ src/containers/feelingsPage/singlePosts/SingleFeelingsPage.tsx
@@ -2,31 +2,38 @@ import { useParams } from 'react-router-dom';
 import { useGetOneFeeling } from '../../../features/queryHooks/queryHooks';
 import FroalaEditorView from 'react-froala-wysiwyg/FroalaEditorView';
 import './SingleFeelingsPage.scss';
+import { Spin } from 'antd';
 
 export const SingleFeelingsPage = () => {
 	const { id } = useParams();
-	const { data: post } = useGetOneFeeling(Number(id));
+	const { data: post, isPending } = useGetOneFeeling(Number(id));
 
 	return (
 		<>
-			<div className="singlePage-block-item-text">
-				<div>
-					<p className="singlePage-block-item-text-title">{post?.title}</p>
-					<p className="feelingPage-block-item-text-date">9 января 2024 года</p>
-					<div className="singlePage-block-item-text-description">
-						<FroalaEditorView
-							model={post?.description ? post.description : 'No description'}
+			{isPending ? (
+				<Spin />
+			) : (
+				<div className="singlePage-block-item-text">
+					<div>
+						<p className="singlePage-block-item-text-title">{post?.title}</p>
+						<p className="feelingPage-block-item-text-date">
+							9 января 2024 года
+						</p>
+						<div className="singlePage-block-item-text-description">
+							<FroalaEditorView
+								model={post?.description ? post.description : 'No description'}
+							/>
+						</div>
+					</div>
+					<div>
+						<img
+							src={`http://localhost:8000/uploads/${post?.image}`}
+							alt={post?.title}
+							className="feelingPage-block-item-text-image"
 						/>
 					</div>
 				</div>
-				<div>
-					<img
-						src={`http://localhost:8000/uploads/${post?.image}`}
-						alt={post?.title}
-						className="feelingPage-block-item-text-image"
-					/>
-				</div>
-			</div>
+			)}
 		</>
 	);
 };
