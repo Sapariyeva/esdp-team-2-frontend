e7356e10d8f367e85000658e69701414be2e43bd
Adilkhan <nucelios@mail.ru>
2024-01-10 21:32:23 +0600

    #104 fixed issues with password

--- src/components/patient/patient_account/profile/PatientProfile.tsx
+++ src/components/patient/patient_account/profile/PatientProfile.tsx
@@ -12,9 +12,11 @@ import {
 function PatientProfile() {
 	const dispatch = useAppDispatch();
 	const errors = useAppSelector((state) => state.users.loginError);
-	const [formKey, setFormKey] = useState(0);
 	const userInfo = useAppSelector((state) => state.users.userInfo);
-
+	const active = useAppSelector((state) => state.users.pagelock);
+	const [serverError, setServerError] = useState<string>('');
+	const [form] = Form.useForm();
+	serverError;
 	const [initialValues, setInitialValues] = useState<IUserEdit>({
 		email: userInfo?.email,
 		name: userInfo?.patient?.name,
@@ -22,12 +24,25 @@ function PatientProfile() {
 		сurrentPassword: '',
 	});
 
-	const active = useAppSelector((state) => state.users.pagelock);
-	const [serverError, setServerError] = useState<string>('');
+	useEffect(() => {
+		if (userInfo) {
+			setInitialValues({
+				email: userInfo.email,
+				name: userInfo.patient?.name || '',
+				password: '',
+				сurrentPassword: '',
+			});
+			setCurrentPasswordEntered(false);
+			setEmailChanged(false);
+		}
+	}, [userInfo]);
+
+	useEffect(() => {
+		form.setFieldsValue(initialValues);
+	}, [initialValues, form]);
 
 	const handleSubmit = async (values: IUserEdit) => {
 		values.сurrentPassword = (values.сurrentPassword ?? '').trim();
-
 		if (values.сurrentPassword === '') {
 			dispatch(
 				updatePatientName({
@@ -45,23 +60,21 @@ function PatientProfile() {
 				})
 			);
 		}
+		form.resetFields();
 	};
 
 	const [passwordValue, setPasswordValue] = useState('');
-	const [CurrentPasswordValue, setCurrentPasswordValue] = useState('');
 	const [currentPasswordEntered, setCurrentPasswordEntered] = useState(false);
 	const [emailValue, setEmailValue] = useState(initialValues.email || '');
 	const [emailChanged, setEmailChanged] = useState(false);
-	serverError;
-	CurrentPasswordValue;
-	emailValue;
+
 	const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setEmailValue(e.target.value);
-		setEmailChanged(true);
+		setEmailChanged(e.target.value !== initialValues.email);
 	};
 
 	const validateEmail = () => {
-		if (emailChanged && !currentPasswordEntered) {
+		if (!currentPasswordEntered && emailChanged) {
 			return Promise.reject('Введите текущий пароль');
 		}
 		return Promise.resolve();
@@ -70,9 +83,15 @@ function PatientProfile() {
 	const handleCurrentPasswordChange = (
 		e: React.ChangeEvent<HTMLInputElement>
 	) => {
-		setCurrentPasswordEntered(e.target.value.trim().length > 0);
-		setCurrentPasswordValue(e.target.value);
+		const currentValue = e.target.value.trim().length > 0;
+		setCurrentPasswordEntered(currentValue);
 	};
+
+	useEffect(() => {
+		setEmailChanged(
+			emailValue !== initialValues.email || currentPasswordEntered
+		);
+	}, [emailValue, initialValues.email, currentPasswordEntered]);
 
 	const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setPasswordValue(e.target.value);
@@ -82,32 +101,25 @@ function PatientProfile() {
 		setServerError(errors?.message as string);
 	}, [errors, active]);
 
-	useEffect(() => {
-		if (active === false) {
-			setFormKey((prevKey) => prevKey + 1);
-			setInitialValues({
-				email: userInfo?.email,
-				name: userInfo?.patient?.name,
-				password: '',
-				сurrentPassword: '',
-			});
-			setServerError('');
-		}
-	}, [userInfo, active]);
-
 	const handleSingleButtonClick = () => {
 		dispatch(changePageLock(true));
 	};
 	const handleSingleButtonClickCancel = () => {
+		form.resetFields();
 		dispatch(changePageLock(false));
 		dispatch(resetErrors());
+	};
+
+	const getErrorsBy = (name: string) => {
+		const error = errors?.errors?.find((error) => error.type === name);
+		return error?.messages.join(',');
 	};
 
 	return (
 		<div>
 			<Layout className={styles.layout}>
 				<Form
-					key={formKey}
+					form={form}
 					name="update-form"
 					className="form"
 					onFinish={handleSubmit}
@@ -115,7 +127,7 @@ function PatientProfile() {
 				>
 					<Row>
 						{!active && (
-							<Col xs={24} sm={7} md={9} lg={9} xl={5}>
+							<Col xs={26} sm={8} md={9} lg={9} xl={7}>
 								<Form.Item>
 									<Button
 										className={styles.btn_confirm}
@@ -128,7 +140,7 @@ function PatientProfile() {
 						)}
 						{active && (
 							<>
-								<Col xs={24} sm={7} md={9} lg={9} xl={5}>
+								<Col xs={24} sm={10} md={9} lg={9} xl={7}>
 									<Form.Item>
 										<Button className={styles.btn_confirm} htmlType="submit">
 											Применить изменения
@@ -156,6 +168,7 @@ function PatientProfile() {
 								className="form-item"
 								name="email"
 								hasFeedback
+								dependencies={['сurrentPassword']}
 								rules={[
 									{
 										validator: validateEmail,
@@ -173,18 +186,16 @@ function PatientProfile() {
 									(errors?.message ===
 									'Пользователь с таким email уже существует'
 										? 'Пользователь с таким email уже существует'
-										: '') ||
-									(passwordValue && !currentPasswordEntered
-										? 'Пожалуйста, введите текущий пароль.'
-										: undefined)
+										: '') || getErrorsBy('email')
 								}
 								validateStatus={
 									(errors?.message ===
 									'Пользователь с таким email уже существует'
 										? 'error'
 										: '') ||
-									(passwordValue && !currentPasswordEntered ? 'error' : '') ||
-									undefined
+									(errors?.errors?.find((error) => error.type === 'email')
+										? 'error'
+										: undefined)
 								}
 							>
 								<Input
@@ -195,7 +206,7 @@ function PatientProfile() {
 								/>
 							</Form.Item>
 						</Col>
-						<Col xs={24} sm={12} md={12} lg={12} xl={12}>
+						<Col xs={24} sm={24} md={12} lg={12} xl={12}>
 							<label className="label">ФИО</label>
 							<Form.Item
 								name="name"
@@ -212,7 +223,7 @@ function PatientProfile() {
 						</Col>
 					</Row>
 					<Row gutter={16}>
-						<Col xs={24} sm={12} md={12} lg={12} xl={12}>
+						<Col xs={24} sm={24} md={12} lg={12} xl={12}>
 							<label className="label">Текущий Пароль</label>
 							<Form.Item
 								name="сurrentPassword"
@@ -236,7 +247,7 @@ function PatientProfile() {
 							</Form.Item>
 						</Col>
 
-						<Col xs={24} sm={12} md={12} lg={12} xl={12}>
+						<Col xs={24} sm={24} md={12} lg={12} xl={12}>
 							<label className="label">Новый пароль</label>
 							<Form.Item
 								name="password"
