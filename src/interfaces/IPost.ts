280e29a4dcdb53240c951b8e7fa3308de58f2a30
Dmitry <mikotsowl22@gmail.com>
2024-01-14 13:04:32 +0600

    #168 Fix interface IPost

--- src/interfaces/IPost.ts
+++ src/interfaces/IPost.ts
@@ -2,11 +2,14 @@ import { UploadFile } from 'antd';
 
 export interface IPost {
 	id: number;
-	psychologistId: number;
 	title: string;
 	description: string;
+	image: string;
+	isPublish: boolean;
+}
+
+export interface IPostCreation extends Omit<IPost, 'image'> {
 	image: {
 		fileList: UploadFile[];
 	};
-	isPublish: boolean;
 }
